eac4c0e8cbd62bb2ef95169643c63b92c859e813
MersoAbreuDev <emerson.solutions@outlook.com>
2023-01-17 22:32:51 -0300

    criação do login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedMaterialModule } from './shared/shared-material/shared-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContinuationRegisterComponent } from './componentes/continuation-register/continuation-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ContinuationRegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedMaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/continuation-register/continuation-register.component.ts
@@ -0,0 +1,34 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-continuation-register',
+  templateUrl: './continuation-register.component.html',
+  styleUrls: ['./continuation-register.component.scss']
+})
+export class ContinuationRegisterComponent implements OnInit{
+  form!:FormGroup;
+  constructor(
+     @Inject(MAT_DIALOG_DATA) public data:any,
+     private fb:FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+  this. initForm();
+
+  }
+
+  initForm(){
+    this.form = this.fb.group({
+      name:[this.data.data?.name, [Validators.required]],
+      email:[this.data.data?.email, [Validators.required]],
+      age:[this.data.data?.age, [Validators.required]],
+      avatar:[null, Validators.required],
+      password:[null, Validators.required],
+      confirmPassword:[null, Validators.required]
+    })
+  }
+
+}
